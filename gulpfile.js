110a8fb4c4c3221239ab77aa1424114938c769d7
galinovivan <9225913@gmail.com>
2017-08-11 16:29:08 +0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+/**
+ * Created by root on 11.08.17.
+ */
+const gulp = require('gulp'),
+      watch = require('gulp-watch'),
+      uglify = require('gulp-uglify'),
+      sourceMaps = require('gulp-sourcemaps'),
+      ccsmin = require('gulp-minify-css'),
+      imageMin = require('gulp-imagemin'),
+      pngQuant = require('imagemin-pngquant'),
+      sass = require('gulp-sass'),
+      autoprefixer = require('gulp-autoprefixer'),
+      concat = require('gulp-concat'),
+      babel = require('gulp-babel');
+
+
+
+
+const path = {
+  build: {
+      js: 'assets/js/dist',
+      img: 'assets/images',
+      css: 'assets/css'
+  },
+    src: {
+      js: 'assets/js/*.js',
+      img: 'assets/images/src/*.*',
+      style: 'assets/scss/style.scss'
+    }
+};
+
+
+
+const config = {
+    server: {
+        baseDir: '/assets/'
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: 'akira'
+};
+
+
+gulp.task('js:build', () => {
+   gulp.src(path.src.js)
+       .pipe(sourceMaps.init())
+       .pipe(babel({
+           presets: ['env']
+       }))
+       .pipe(concat('bundle.js'))
+       .pipe(uglify())
+       .pipe(sourceMaps.write())
+       .pipe(gulp.dest(path.build.js))
+});
+
+gulp.task('js:prod', () => {
+    gulp.src(path.src.js)
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(concat('bundle.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(path.build.js))
+});
+
+
+gulp.task('style:build', () => {
+   gulp.src(path.src.style)
+       .pipe(sourceMaps.init())
+       .pipe(sass())
+       .pipe(autoprefixer())
+       .pipe(ccsmin())
+       .pipe(sourceMaps.write())
+       .pipe(gulp.dest(path.build.css))
+});
+
+gulp.task('style:prod', () => {
+    gulp.src(path.src.style)
+        .pipe(sass())
+        .pipe(autoprefixer)
+        .pipe(cssmin)
+        .pipe(gulp.dest(path.build.css))
+});
+
+gulp.task('image:build', () => {
+   gulp.src(path.src.img)
+       .pipe(imageMin({
+           progressive: true,
+           svgoPlugins: [{ removeViewBox: false }],
+           use: [pngQuant()],
+           interplaced: true
+       }))
+       .pipe(gulp.dest(path.build.img))
+});
+
+gulp.task('prod:build', () => {
+    gulp.start('js:prod');
+    gulp.start('style:prod');
+});
